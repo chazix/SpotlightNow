ca07604c04fb85364a563deb6f998ae263aa5b2e
chase <m11hut@gmail.com>
2025-10-28 01:08:05 -0700

    download-task.ps1: attempt to grab spotlight images every 1 hour indefinitely

--- download-task.ps1
+++ download-task.ps1
@@ -1,10 +1,9 @@
 # Action: run spotlight-now.exe download
 $Action = New-ScheduledTaskAction -Execute "C:\ProgramData\SpotlightNow\spotlight-now.exe" -Argument "download" -WorkingDirectory "C:\ProgramData\SpotlightNow"
 
-# Trigger: start once today, repeat every 1 hours for 1 day (auto-renews daily)
+# Trigger: start once today, repeat every 1 hours indefinitely
 $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date `
-    -RepetitionInterval (New-TimeSpan -Hours 1) `
-    -RepetitionDuration (New-TimeSpan -Days 1)
+    -RepetitionInterval (New-TimeSpan -Hours 1)
 
 # Run as SYSTEM with highest privileges
 $Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
